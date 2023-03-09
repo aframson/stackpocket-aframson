807a3f1d4db9dc4a707fbd09a09f8671c5a4805c
Richard Obiri <obiriaframson@gmail.com>
2023-03-09 22:16:15 +0000

    span formu

---
+++ span.js
@@ -0,0 +1,20 @@
+
+
+export function span(n,type){
+    let res;
+    if (type === 3) {
+      if (n === 0) {
+        res = 10 - n
+   }else{
+        res = 6 * n * (10 - n)
+   }
+    }else{
+      if (n === 0) {
+        res = 10 - n
+      }else{
+        res = (10 - n) * 2
+       }
+    }
+  
+    return res
+}
