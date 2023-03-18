55d8d748b1f353c83fa29ed7e2a6242a840fa7f2
Richard Obiri <obiriaframson@gmail.com>
2023-03-18 15:08:29 +0000

    this

---
+++ aframson/app.js
@@ -0,0 +1,8 @@
+array = [1,'two',3,4,5,'six','seven',8,'nine',10]
+console.log(array);
+//slicing with only the first parameter
+var newArray = array.slice(4);
+console.log(newArray);
+//slicing with both the paramters
+var newArray2 = array.slice(4,7);
+console.log(newArray2);
