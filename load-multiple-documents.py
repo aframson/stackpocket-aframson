72bb313c514605ebec100bb935b87c865e542146
Richard Obiri <obiriaframson@gmail.com>
2024-06-23 02:10:00 +0000

    changed

--- load-multiple-documents.py
+++ load-multiple-documents.py
@@ -19,3 +19,13 @@ def load_document_data(source_dir: str) -> List:
     all_files = os.listdir(source_dir)
     documents = [load_single_document_data(f"{source_dir}/{file_path}") for file_path in tqdm(all_files) if file_path[-4:] in ['.txt', '.pdf', '.csv']]
     return [doc for doc in documents if doc is not None]
+
+
+SOURCE_DIRECTORY = './knowledgebase/'
+print(f"Loading... documents from {SOURCE_DIRECTORY}...")
+documents = load_document_data(SOURCE_DIRECTORY)
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=10)
+text_chunks = text_splitter.split_documents(documents)
+print(f"Loaded {len(documents)} documents from {SOURCE_DIRECTORY}")
+print(f"Split into {len(text_chunks)} chunks of text")
